3dec41d4adff8821eedadc6974d228d9fe8d3aa5
Michał Danek <danek.frontend@gmail.com>
2025-03-12 19:39:32 +0100

    Extend data in mocked database

--- src/app/shared/services/in-memory-data.service.ts
+++ src/app/shared/services/in-memory-data.service.ts
@@ -25,6 +25,40 @@ export class InMemoryDataService implements InMemoryDbService {
           },
         ],
       },
+      {
+        id: 'vvv1410',
+        name: 'Justine Owens',
+        department: 'Sales',
+        status: 'ACTIVE',
+        email: 'justine@tequipy.com',
+        equipments: [
+          {
+            id: 'aaa1232256',
+            name: 'MacBook Pro',
+          },
+          {
+            id: 'aaa123213456',
+            name: 'Xiaomi Mi 3',
+          },
+          {
+            id: 'aaa12976556',
+            name: 'Dell 200XP',
+          },
+        ],
+      },
+      {
+        id: 'vvv1832',
+        name: 'Amrita Blackmore',
+        department: 'Marketing',
+        status: 'ACTIVE',
+        email: 'amrita@tequipy.com',
+        equipments: [
+          {
+            id: 'aaa091572256',
+            name: 'MacBook Set',
+          },
+        ],
+      },
     ];
 
     return { employees };
